21b955ad30183099776b21bcc552d6e7cef9f4a1
J Emory Parker <eparker@postandcourier.com>
2016-03-23 15:26:25 -0400

    added semantic ui styles

--- package.js
+++ package.js
@@ -1,8 +1,8 @@
 Package.describe({
-  name: 'babrahams:editable-text-wysiwyg-bootstrap-3',
-  summary: 'Extends the babrahams:editable-text package to include a wysiwyg editor that uses bootstrap-3',
-  version: '0.5.10',
-  git: 'https://github.com/jackadams/meteor-editable-text-wysiwyg-bootstrap-3.git'
+  name: 'postandcourier:editable-text-wysiwyg-semantic-ui',
+  summary: 'Extends the babrahams:editable-text package to include a wysiwyg editor that uses Semantic UI',
+  version: '0.1.0',
+  git: 'https://github.com/postandcourier/meteor-editable-text-wysiwyg-bootstrap-3.git'
 });
 
 Package.onUse(function(api) {
